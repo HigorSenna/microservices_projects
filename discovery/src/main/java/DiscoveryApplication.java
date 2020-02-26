07820d2939554b6942c1ac08145221ee35b93aee
s2it_hchaves <s2it_hchaves@uolinc.com>
2020-02-26 08:24:43 -0300

    Ajustes no projeto

--- discovery/src/main/java/DiscoveryApplication.java
+++ discovery/src/main/java/DiscoveryApplication.java
@@ -1,9 +1,12 @@
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaServer
+@ComponentScan("br.com.microservices")
 public class DiscoveryApplication {
 
     public static void main(String[] args) {
--- gateway/src/main/java/GatewayApplication.java
+++ gateway/src/main/java/GatewayApplication.java
@@ -2,10 +2,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableZuulProxy
+
+
+@EnableZuulProxy// cria uma rota por 'baixo dos panos' que permite que eu acesse o course, através do gateway sem precisar de implementação,
+// ex: http://localhost:8080/gateway/course/v1/admin/courses
 @EnableEurekaClient
+@ComponentScan("br.com.microservices")
 public class GatewayApplication {
 
     public static void main(String[] args) {
